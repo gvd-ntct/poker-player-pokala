a7a4f2598d5a8155b3e65aabb653e45fb383bc7e
Gerrit van Doorn <gerrit.vandoorn@netscout.com>
2016-07-21 11:44:08 -0700

    More aggresive call strategy

--- src/main/scala/org/leanpoker/player/Player.scala
+++ src/main/scala/org/leanpoker/player/Player.scala
@@ -5,14 +5,14 @@ import com.google.gson.JsonElement
 object Player {
 
   val VERSION = "Pokala never fold"
-  
+
   val random = new scala.util.Random()
   def betRequest(request: JsonElement) = {
     val state = Parsing.parseGame(request)
     val bet = state.fold(0) { s =>
       val num = random.nextInt(10)
-      if (num < 8) {
-        math.min(s.minimumCallAmount, s.myStack)
+      if (num < 5) {
+        math.min(s.minimumCallAmountAggresive, s.myStack)
       } else {
         math.min(s.minimumRaiseAmount + 5, s.myStack)
       }
