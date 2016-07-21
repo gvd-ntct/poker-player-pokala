3775b205db57be3200c1a2b4f232835cd9ef40be
Gerrit van Doorn <gerrit.vandoorn@netscout.com>
2016-07-21 10:47:02 -0700

    Initial random logic
    Just have some random fold, check, raise logic.

--- src/main/scala/org/leanpoker/player/Player.scala
+++ src/main/scala/org/leanpoker/player/Player.scala
@@ -6,7 +6,19 @@ object Player {
 
   val VERSION = "Pokala initial data model"
 
-  def betRequest(request: JsonElement) = 0
+  val random = new scala.util.Random()
+
+  def betRequest(request: JsonElement): Unit = {
+    val state = Parsing.parseGame(request)
+    state.fold(0) { s =>
+      val num = random.nextInt(10)
+      if (num < 3) {
+        math.min(s.minimumCallAmount, s.myStack)
+      } else if (num == 0) {
+        math.min(s.minimumRaiseAmount + 2, s.myStack)
+      } else 0
+    }
+  }
 
   def showdown(game: JsonElement) {
 
