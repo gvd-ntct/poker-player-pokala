de83e1259fd6909284f499e1589a27cca8fb2c58
Gerrit van Doorn <gerrit.vandoorn@netscout.com>
2016-07-21 13:38:30 -0700

    Include hole odds and average with total odds

--- src/main/scala/org/leanpoker/player/Player.scala
+++ src/main/scala/org/leanpoker/player/Player.scala
@@ -13,10 +13,12 @@ object Player {
     val bet = state.fold(0) { s =>
       val rem = Poker.cards diff s.myCombinedCards
       val deck = Deck(rem)
+      val preFlopOdds = Poker.odds(s.myHoleCards, List(), deck, s.players.size, 100)
       val odds = Poker.odds(s.myHoleCards, s.communityCards, deck, s.players.size, 100)
+      val combinedOdds = (preFlopOdds._1 + odds._1) / 2.0
       val randomOdds = 1.0 / s.players.size
-      if (odds._1 == 0.0) 0
-      else if (odds._1 > randomOdds) math.min(s.minimumRaiseAmount + 5, s.myStack)
+      if (combinedOdds == 0.0) 0
+      else if (combinedOdds > randomOdds) math.min(s.minimumRaiseAmount + 5, s.myStack)
       else math.min(s.minimumCallAmountAggresive, s.myStack)
     }
     System.err.println(s"Our bet $bet")
