0d5e1b1c2822660ba5d667a30790d2a6a3410ce3
Gerrit van Doorn <gerrit.vandoorn@netscout.com>
2016-07-21 11:32:19 -0700

    Never fold

--- src/main/scala/org/leanpoker/player/Player.scala
+++ src/main/scala/org/leanpoker/player/Player.scala
@@ -4,17 +4,18 @@ import com.google.gson.JsonElement
 
 object Player {
 
-  val VERSION = "Pokala initial data model"
+  val VERSION = "Pokala never fold"
+  
   val random = new scala.util.Random()
   def betRequest(request: JsonElement) = {
     val state = Parsing.parseGame(request)
     val bet = state.fold(0) { s =>
       val num = random.nextInt(10)
-      if (num < 7) {
+      if (num < 8) {
         math.min(s.minimumCallAmount, s.myStack)
-      } else if (num == 0) {
-        math.min(s.minimumRaiseAmount + 2, s.myStack)
-      } else 0
+      } else {
+        math.min(s.minimumRaiseAmount + 5, s.myStack)
+      }
     }
     System.err.println(s"Our bet $bet")
     bet
