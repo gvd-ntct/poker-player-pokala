718bb0f4c8b3d9cea06d4fdcbd193e056e262f3d
Gerrit van Doorn <gerrit.vandoorn@netscout.com>
2016-07-21 14:33:17 -0700

    Change back to old behaviour. No limiting

--- src/main/scala/org/leanpoker/player/Player.scala
+++ src/main/scala/org/leanpoker/player/Player.scala
@@ -17,8 +17,8 @@ object Player {
       val odds = Poker.odds(s.myHoleCards, s.communityCards, deck, s.players.size - 1, 100)
       val combinedOdds = (preFlopOdds._1 + odds._1) / 2.0
       val randomOdds = 1.0 / s.players.size
-      if (combinedOdds == 0.0 && s.myBet < 50) 0
-      else if (combinedOdds > randomOdds && s.myBet < (s.myStack / 2.0)) math.min(s.minimumRaiseAmount + 5, s.myStack)
+      if (combinedOdds == 0.0) 0
+      else if (combinedOdds > randomOdds) math.min(s.minimumRaiseAmount + 5, s.myStack)
       else math.min(s.minimumCallAmountAggresive, s.myStack)
     }
     System.err.println(s"Our bet $bet")
